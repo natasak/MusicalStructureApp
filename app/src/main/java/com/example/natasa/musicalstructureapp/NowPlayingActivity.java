938af2eed59ebac2e8c9abf76c0d78f8ca393bb4
Natasa Mori <natasa@the-mori.com>
2018-03-21 10:53:57 +0100

    Added buttons for going to another activity.

--- app/src/main/java/com/example/natasa/musicalstructureapp/NowPlayingActivity.java
+++ app/src/main/java/com/example/natasa/musicalstructureapp/NowPlayingActivity.java
@@ -2,11 +2,16 @@ package com.example.natasa.musicalstructureapp;
 
 /** Gif image from: https://giphy.com/gifs/tony-sirico-206LP4kRMWd0Y  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NowPlayingActivity extends AppCompatActivity {
+
+    private Button childrenButton, programmingButton, homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +19,47 @@ public class NowPlayingActivity extends AppCompatActivity {
 
         setContentView(R.layout.now_playing);
 
-        // Get the data from previous activity and display it
+        childrenButton = (Button) findViewById(R.id.children_button);
+        programmingButton = (Button) findViewById(R.id.programming_button);
+        homeButton = (Button) findViewById(R.id.home_button);
+
+        //Set a clicklistener on that Button for children playlist
+        childrenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the ChildrenActivity
+                Intent childrenButtonIntent = new Intent(NowPlayingActivity.this, ChildrenActivity.class);
+
+                //Start the new activity
+                startActivity(childrenButtonIntent);
+            }
+        });
+
+        //Set a clicklistener on that Button for programming playlist
+        programmingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the ChildrenActivity
+                Intent programmingButtonIntent = new Intent(NowPlayingActivity.this, ProgrammingActivity.class);
+
+                //Start the new activity
+                startActivity(programmingButtonIntent);
+            }
+        });
+
+        //Set a clicklistener on that Button for home
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the ChildrenActivity
+                Intent homeButtonIntent = new Intent(NowPlayingActivity.this, MainActivity.class);
+
+                //Start the new activity
+                startActivity(homeButtonIntent);
+            }
+        });
+
+        // Get the data from previous activity (selected song in the playlist) and display it
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             TextView myTextView = (TextView)findViewById(R.id.key_artist_text_view);
